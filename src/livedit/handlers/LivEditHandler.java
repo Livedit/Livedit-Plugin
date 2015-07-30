110adf5b45e29aee259b4bdcfc5263d373e6b352
2jordan3 <jordan___23@naver.com>
2015-07-31 07:06:48 +0900

    Focus-out save 추가

--- src/livedit/handlers/LivEditHandler.java
+++ src/livedit/handlers/LivEditHandler.java
@@ -2,10 +2,20 @@
 
 package livedit.handlers;
 
+import java.io.File;
+
+import javax.swing.text.PlainView;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.webbitserver.handler.StaticFileHandler;
 import org.webbitserver.WebServer;
 import org.webbitserver.WebServers;
+import org.eclipse.core.resources.ResourcesPlugin;
 
 public class LivEditHandler extends AbstractHandler{
 	protected boolean WBind = false;
@@ -18,8 +28,19 @@ public class LivEditHandler extends AbstractHandler{
 	public Object execute(ExecutionEvent event)  {
 		try {
 			if(!WBind){
-	        	webServer = WebServers.createWebServer(63312)
+				/*String path = ResourcesPlugin.getWorkspace().getRoot().getLocation().toString();
+				String check = Display.getCurrent().getActiveShell().toString();
+				String extension = check.replace("Shell {Resource - ", "").replace(" - Eclipse Platform}", "");
+				
+				System.out.println(path+"/"+extension);
+				
+				File fPath = new File(path+"/"+extension);*/
+												
+				
+				
+				webServer = WebServers.createWebServer(63312)
 	        			.add("/", new Websocket())
+	        			//.add("/"+extension, new HttpHandlerImpl())
 	                    .start()
 	                    .get();
 	        	WBind = true;
--- src/livedit/handlers/Websocket.java
+++ src/livedit/handlers/Websocket.java
@@ -29,189 +29,228 @@ public class Websocket extends BaseWebSocketHandler {
 	
 	public void onOpen(WebSocketConnection connection) {
 		System.out.println("connected");
-		
+	
 		Display.getDefault().asyncExec(new Runnable(){
-			@Override
-            public void run() {
-							
-				//Display.getCurrent().addFilter(SWT.MouseDown, new Listener(){		//마우스는 lineNumber가 늦음
-				Display.getCurrent().addFilter(SWT.FOCUSED, new Listener(){
-				
+				@Override
+	            public void run() {
+								
+					//Display.getCurrent().addFilter(SWT.MouseDown , new Listener(){		//마우스는 lineNumber가 늦음
+					Display.getCurrent().addFilter(SWT.FOCUSED, new Listener(){
 					
-					@Override
-					public void handleEvent(Event event) {
-						
-						IWorkbench wb = PlatformUI.getWorkbench();
-						IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
-						IWorkbenchPage page = win.getActivePage();
-						IEditorPart editor = page.getActiveEditor();
-						ITextEditor tEditor = (ITextEditor)editor;
-						IDocumentProvider dp = tEditor.getDocumentProvider();
-						IDocument du = dp.getDocument(tEditor.getEditorInput());
-						
-						//System.out.println(du.get());				//editor 내용 전체 긁어오기
-						//System.out.println(" " + tEditor.getTitle());	//파일명+확장자
-						
-						try{
+						@Override
+						public void handleEvent(Event event) {
+							
+							IWorkbench wb = PlatformUI.getWorkbench();
+							IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+							IWorkbenchPage page = win.getActivePage();
+							IEditorPart editor = page.getActiveEditor();
+							ITextEditor tEditor = (ITextEditor)editor;
+							IDocumentProvider dp = tEditor.getDocumentProvider();
+							IDocument du = dp.getDocument(tEditor.getEditorInput());
+							
+							//System.out.println(du.get());				//editor 내용 전체 긁어오기
+							//System.out.println(" " + tEditor.getTitle());	//파일명+확장자
+							
+							try{
+								
+								check = Display.getCurrent().getActiveShell().toString();
+								System.out.println("check : " + check);
+								
+								if( check.contains(".html") ) {
+										
+									ITextSelection textSelection = (ITextSelection) editor
+									        .getSite().getSelectionProvider().getSelection();
+									int offset = textSelection.getOffset();
+									int lineNumber = du.getLineOfOffset(offset);
+									
+									int tempLine = textSelection.getStartLine();
+									int column = 0;
+									
+									column = textSelection.getOffset() - du.getLineOffset(tempLine);
+									
+									System.out.println("Column : " + column);
+									
+									int length = 0;
+									
+									for(int i = 0; i <= lineNumber; i++){
+										length += du.getLineLength(i);
+									}
+									
+									String ahtml = du.get(0, length);
+									//ahtml = ahtml.replaceAll("(\t|\r\n|\n)", "");
+									Document doc = Jsoup.parse(ahtml);
+									
+									String lineDoc = du.get(length-du.getLineLength(lineNumber), du.getLineLength(lineNumber));
+									
+									lineDoc = lineDoc.toLowerCase();
+									lineDoc = lineDoc.replaceAll("(\t|\r\n|\n)", "");
+									
+									Elements links = null;
+									
+									int j = 0;
+									
+									String tagName = null;
+									
+									System.out.println("lineDoc : " + lineDoc);
+									
+									for(; j < lineDoc.length(); j++){
+										System.out.println("lineDoc : " + lineDoc.charAt(j));
+										if(lineDoc.charAt(j) == '>' || lineDoc.charAt(j) == ' '){
+											break;
+										}
+									}					
+									
+									System.out.println("lineDoc j : " + j);
+									
+									tagName = lineDoc.substring(1, j);
+									
+									if(tagName.contains("/")){
+										tagName = tagName.replace("/","");
+									}
+								
+									System.out.println("tagName : " + tagName);
+									
+									links = doc.select(tagName);
+									
+									System.out.println(links.size());
+									
+									Element linkTemp = links.get(links.size() - 1);
+									
+									System.out.println("selector1 : " + linkTemp.cssSelector());
+									
+									onMessage(connection,	"{ \"command\" : \"inspect\", \"nodeSelector\" : \"" + linkTemp.cssSelector() + "\"}");
+									
+									System.out.println("lineNumber : " + (lineNumber+1));
+									
+									System.out.println("line data : " + du.get(length-du.getLineLength(lineNumber), du.getLineLength(lineNumber)));
+									
+									System.out.println("----------------------------------------");
+								}
+								
+							} catch(Exception e){
+								e.printStackTrace();
+							}
+						}
+					});
+					
+					Display.getCurrent().addFilter(SWT.FocusOut, new Listener(){
+						@Override
+						public void handleEvent(Event event) {
+							IWorkbench wb = PlatformUI.getWorkbench();
+							IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+							IWorkbenchPage page = win.getActivePage();
+							IEditorPart editor = page.getActiveEditor();
+							ITextEditor tEditor = (ITextEditor)editor;
+							IDocumentProvider dp = tEditor.getDocumentProvider();
+							IDocument du = dp.getDocument(tEditor.getEditorInput());
+							
+							page.saveEditor(editor, false);
+							
+							try {
+							
+								if ( check.contains(".html") ){
+									
+									String ahtml = du.get(0, du.getLength());
+									ahtml = ahtml.replaceAll("(\t|\r\n|\n)", "");
+									Document doc = Jsoup.parse(ahtml);
+									
+									
+									
+									String html = du.get().replaceAll("(\t|\r\n|\n)", "");
+									html = html.replaceAll("\"", "'");
+									
+									System.out.println("why?");
+									onMessage(connection,	"{ \"command\" : \"injectJavascript\"}");
+									
+								} else if ( check.contains(".js")){
+	
+									String js = du.get().replaceAll("(\t|\r\n|\n)", "");
+									js = js.replaceAll("\"", "'");
+																	
+									onMessage(connection,	"{ \"command\" : \"injectJavascript\"}");
+									
+								} else if (check.contains(".css")){
+									
+									String css = du.get().replaceAll("(\t|\r\n|\n)", "");
+									css = css.replaceAll("\"", "'");
+									
+									onMessage(connection,	"{ \"command\" : \"injectCss\", \"code\" : \"" + css + "\"}");
+									
+								}
+							} catch (Exception e){
+								e.printStackTrace();
+							}
+						}
+					});
+					
+					Display.getCurrent().addFilter(SWT.KeyDown, new Listener(){
+						@Override
+						public void handleEvent(Event event) {
+							String extension = null;
+							
 							
 							check = Display.getCurrent().getActiveShell().toString();
+							extension = check.replace("Shell {Resource - ", "").replace(" - Eclipse Platform}", "");
 							
-							if( check.contains(".html") ) {
-									
-								ITextSelection textSelection = (ITextSelection) editor
-								        .getSite().getSelectionProvider().getSelection();
-								int offset = textSelection.getOffset();
-								int lineNumber = du.getLineOfOffset(offset);
+							IWorkbench wb = PlatformUI.getWorkbench();
+							IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+							IWorkbenchPage page = win.getActivePage();
+							IEditorPart editor = page.getActiveEditor();
+							ITextEditor tEditor = (ITextEditor)editor;
+							IDocumentProvider dp = tEditor.getDocumentProvider();
+							IDocument du = dp.getDocument(tEditor.getEditorInput());
+							
+							try {
+															
+								/*String msg = Character.toString(event.character);
+								onMessage(connection, msg);*/
 								
-								int length = 0;
+								//System.out.println(event.keyCode);
 								
-								for(int i = 0; i <= lineNumber; i++){
-									length += du.getLineLength(i);
-								}
-								
-								String ahtml = du.get(0, length);
-								//ahtml = ahtml.replaceAll("(\t|\r\n|\n)", "");
-								Document doc = Jsoup.parse(ahtml);
-								
-								String lineDoc = du.get(length-du.getLineLength(lineNumber), du.getLineLength(lineNumber));
-								
-								lineDoc = lineDoc.toLowerCase();
-								lineDoc = lineDoc.replaceAll("(\t|\r\n|\n)", "");
-								
-								Elements links = null;
-								
-								int j = 0;
-								
-								String tagName = null;
-								
-								for(; j < lineDoc.length(); j++){
-									if(lineDoc.charAt(j) == '>' || lineDoc.charAt(j) == ' '){
+								switch(event.keyCode){
+									case 16777219:  case 16777220:	//case 16777217:	case 16777218:								//방향키
+									case 65536:																						//alt
+									//case 262144:																					//ctrl
+									case 131072:																					//shift
+									case 9:																							//tab
+									case 16777298:																					//capslock
+									case 27:																						//esc
+									case 16777226:	case 16777227:	case 16777228:	case 16777229:	case 16777230:	case 16777231:	//f1~f6
+									case 16777232:	case 16777233:	case 16777234:	case 16777235:	case 16777236:	case 16777237:	//f7~f12
+									case 16777300:																					//scroll lock
+									case 16777301:																					//pause break
+									case 16777221:			 																		//page up
+									case 16777222:																					//page down
 										break;
+									default: {
+										
+										if ( check.contains(".html") ){
+											
+											String ahtml = du.get(0, du.getLength());
+											ahtml = ahtml.replaceAll("(\t|\r\n|\n)", "");
+											Document doc = Jsoup.parse(ahtml);
+											
+											String html = du.get().replaceAll("(\t|\r\n|\n)", "");
+											html = html.replaceAll("\"", "'");
+																						
+											onMessage(connection,	"{ \"command\" : \"insert\", \"nodeSelector\" : \"html\", \"code\" : \"" + html + "\"}");
+											//onMessage(connection,	"{ \"command\" : \"injectJavascript\", \"code\" : \"" + script + "\"}");
+											
+										} 
 									}
-								}					
-								
-								tagName = lineDoc.substring(1, j);
-								
-								if(tagName.contains("/")){
-									tagName = tagName.replace("/","");
 								}
+								
+								
+							} catch (Exception e) {
+								e.printStackTrace();
+							} 
 							
-								System.out.println("tagName : " + tagName);
-								
-								links = doc.select(tagName);
-								
-								System.out.println(links.size());
-								
-								Element linkTemp = links.get(links.size() - 1);
-								
-								System.out.println("selector1 : " + linkTemp.cssSelector());
-								
-								onMessage(connection,	"{ \"command\" : \"inspect\", \"nodeSelector\" : \"" + linkTemp.cssSelector() + "\"}");
-								
-								System.out.println("lineNumber : " + (lineNumber+1));
-								
-								System.out.println("line data : " + du.get(length-du.getLineLength(lineNumber), du.getLineLength(lineNumber)));
-								
-								System.out.println("----------------------------------------");
-							}
-							
-						}catch(Exception e){
-							System.out.println("Exception");
-						}
-					}
-				});
+						}	
+					});
 				
-				Display.getCurrent().addFilter(SWT.KeyDown, new Listener(){
-					@Override
-					public void handleEvent(Event event) {
-						String extension = null;
-						
-						check = Display.getCurrent().getActiveShell().toString();
-						extension = check.replace("Shell {Resource - ", "").replace(" - Eclipse Platform}", "");
-						
-						IWorkbench wb = PlatformUI.getWorkbench();
-						IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
-						IWorkbenchPage page = win.getActivePage();
-						IEditorPart editor = page.getActiveEditor();
-						ITextEditor tEditor = (ITextEditor)editor;
-						IDocumentProvider dp = tEditor.getDocumentProvider();
-						IDocument du = dp.getDocument(tEditor.getEditorInput());
-						
-						try {
-														
-							/*String msg = Character.toString(event.character);
-							onMessage(connection, msg);*/
-							
-							//System.out.println(event.keyCode);
-							
-							switch(event.keyCode){
-								case 16777219:  case 16777220:	//case 16777217:	case 16777218:								//방향키
-								case 65536:																						//alt
-								//case 262144:																					//ctrl
-								case 131072:																					//shift
-								case 9:																							//tab
-								case 16777298:																					//capslock
-								case 27:																						//esc
-								case 16777226:	case 16777227:	case 16777228:	case 16777229:	case 16777230:	case 16777231:	//f1~f6
-								case 16777232:	case 16777233:	case 16777234:	case 16777235:	case 16777236:	case 16777237:	//f7~f12
-								case 16777300:																					//scroll lock
-								case 16777301:																					//pause break
-								case 16777221:			 																		//page up
-								case 16777222:																					//page down
-									break;
-								default: {
-									
-									if ( check.contains(".html") ){
-										
-										String ahtml = du.get(0, du.getLength());
-										ahtml = ahtml.replaceAll("(\t|\r\n|\n)", "");
-										Document doc = Jsoup.parse(ahtml);
-										
-										Elements scriptElement = doc.getElementsByTag("script");
-										
-										String script = null;
-										
-										for (Element element :scriptElement ){                
-									        for (DataNode node : element.dataNodes()) {
-									        	script = ""+node.getWholeData();
-									        }            
-										}
-										
-										String html = du.get().replaceAll("(\t|\r\n|\n)", "");
-										html = html.replaceAll("\"", "'");
-										script = script.replaceAll("\"", "'");
-										
-										onMessage(connection,	"{ \"command\" : \"insert\", \"nodeSelector\" : \"html\", \"code\" : \"" + html + "\"}");
-										onMessage(connection,	"{ \"command\" : \"injectJavascript\", \"code\" : \"" + script + "\"}");
-										
-									} else if ( check.contains(".js")){
-
-										String js = du.get().replaceAll("(\t|\r\n|\n)", "");
-										js = js.replaceAll("\"", "'");
-										
-										onMessage(connection,	"{ \"command\" : \"externalJavascript\", \"fileName\" : \"" + extension + "\", \"code\" : \"" + js + "\"}");
-										
-									} else if (check.contains(".css")){
-										
-										String css = du.get().replaceAll("(\t|\r\n|\n)", "");
-										css = css.replaceAll("\"", "'");
-										
-										onMessage(connection,	"{ \"command\" : \"externalCss\", \"fileName\" : \"" + extension + "\", \"code\" : \"" + css + "\"}");
-										
-									}
-								}
-							}
-							
-							
-						} catch (Exception e) {
-							System.out.println("Exception");
-						} 
-						
-					}	
-				});
-			
-			}
-		});		
+				}
+			});
+		
     }
 
     public void onClose(WebSocketConnection connection) {
@@ -228,4 +267,20 @@ public class Websocket extends BaseWebSocketHandler {
     public void onMessage(WebSocketConnection connection, byte[] msg) {
     	connection.send(msg);
     }
+    
+	/*public void save(final IEditorPart editor){
+		
+		Display.getCurrent().asyncExec(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				ProgressMonitorPart pmp = (ProgressMonitorPart)Display.getCurrent().getMonitors();
+				System.out.println(Display.getCurrent().getMonitors());
+				//editor.doSaveAs();
+				editor.doSave(progressMonitor);
+			}
+			
+		});
+	}*/
 }
